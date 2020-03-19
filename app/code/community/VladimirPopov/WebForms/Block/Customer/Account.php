fdd1d1484b14398190b0cb0c789916f34e299edd
Will Furphy <will@techniquedm.com>
2020-03-20 01:49:41 +1100

    Fix mail provider issues.

--- app/code/community/VladimirPopov/WebForms/Block/Customer/Account.php
+++
@@ -1,6 +0,0 @@
-<?php
-class VladimirPopov_WebForms_Block_Customer_Account
-    extends Mage_Core_Block_Template
-{
-
-}
--- lib/Webforms/mpdf.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-if (file_exists(__DIR__ . '/mpdf/mpdf.php'))
-    @require_once __DIR__ . '/mpdf/mpdf.php';
